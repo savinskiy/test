7536125c7e9d6cc42ee23727c6173b4f0a2c1bb4
DmitriyS <DSavin@yotateam.com>
2015-10-06 22:53:27 +0300

    Checker

--- src/ru/ncedu/java/tasks/checker/MyChecker.java
+++ src/ru/ncedu/java/tasks/checker/MyChecker.java
@@ -3,13 +3,53 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package ru.ncedu.java.tasks.checker;
+
+import java.util.*;
+import java.util.regex.*;
 
 /**
  *
  * @author DimaZ
  */
-public class MyChecker {
-    
+public class MyChecker implements Checker {
+
+    @Override
+    public Pattern getPLSQLNamesPattern() {
+        String pattern = "^[a-zA-Z][a-zA-z0-9_\\$]{0,29}$";
+        return Pattern.compile(pattern);
+    }
+
+    @Override
+    public Pattern getHrefURLPattern() {
+        String pattern = "\"<\\\\s*a\\\\s+href\\\\s*"
+                + "=\\\\s*(\\\"[^\\\"]*\\\"|[^\\\"\\\\s]+)\\\\s*/?>\"";
+        return Pattern.compile(pattern, Pattern.CASE_INSENSITIVE);
+    }
+
+    @Override
+    public Pattern getEMailPattern() {
+        String pattern = "^([a-zA-Z0-9][a-zA-Z0-9_.\\-]{0,20}[a-zA-Z0-9]|[a-zA-Z0-9])"
+                + "[@]([a-zA-Z0-9]\\.|[a-zA-Z0-9][a-zA-Z0-9\\-]*[a-zA-Z0-9]\\.)+"
+                + "(ru|com|net|org)$";
+        return Pattern.compile(pattern);
+    }
+
+    @Override
+    public boolean checkAccordance(String inputString, Pattern pattern) throws IllegalArgumentException {
+        if (inputString == null && pattern == null) {
+            return true;
+        }
+        return pattern.matcher(inputString).find();
+    }
+
+    @Override
+    public List<String> fetchAllTemplates(StringBuffer inputString, Pattern pattern) throws IllegalArgumentException {
+        List<String> result = new ArrayList<String>();
+        Matcher m = pattern.matcher(inputString);
+        while (m.find()) {
+            result.add(m.group());
+        }
+        return result;
+    }
 }
