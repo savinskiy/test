0082381126a38ff886e4df7ba259de349160ada5
DmitriyS <DSavin@yotateam.com>
2015-10-07 16:02:03 +0300

    xml: parseroot

--- src/ru/ncedu/java/tasks/xml/MySimpleXML.java
+++ src/ru/ncedu/java/tasks/xml/MySimpleXML.java
@@ -16,11 +16,8 @@ import org.w3c.dom.*;
 import javax.xml.transform.dom.*;
 import javax.xml.transform.stream.*;
 
-/**
- *
- * @author DimaZ
- */
 public class MySimpleXML implements SimpleXML {
+    private String root = null;
 
     @Override
     public String createXML(String tagName, String textNode) {
@@ -50,72 +47,20 @@ public class MySimpleXML implements SimpleXML {
 
             DefaultHandler handler = new DefaultHandler() {
 
-                boolean bfname = false;
-                boolean blname = false;
-                boolean bnname = false;
-                boolean bsalary = false;
-
+                @Override
                 public void startElement(String uri, String localName, String qName,
                         Attributes attributes) throws SAXException {
-
-                    System.out.println("Start Element :" + qName);
-
-                    if (qName.equalsIgnoreCase("FIRSTNAME")) {
-                        bfname = true;
+                    if (root == null) {
+                        root = qName;
                     }
-
-                    if (qName.equalsIgnoreCase("LASTNAME")) {
-                        blname = true;
-                    }
-
-                    if (qName.equalsIgnoreCase("NICKNAME")) {
-                        bnname = true;
-                    }
-
-                    if (qName.equalsIgnoreCase("SALARY")) {
-                        bsalary = true;
-                    }
-
                 }
-
-                public void endElement(String uri, String localName,
-                        String qName) throws SAXException {
-
-                    System.out.println("End Element :" + qName);
-
-                }
-
-                public void characters(char ch[], int start, int length) throws SAXException {
-
-                    if (bfname) {
-                        System.out.println("First Name : " + new String(ch, start, length));
-                        bfname = false;
-                    }
-
-                    if (blname) {
-                        System.out.println("Last Name : " + new String(ch, start, length));
-                        blname = false;
-                    }
-
-                    if (bnname) {
-                        System.out.println("Nick Name : " + new String(ch, start, length));
-                        bnname = false;
-                    }
-
-                    if (bsalary) {
-                        System.out.println("Salary : " + new String(ch, start, length));
-                        bsalary = false;
-                    }
-
-                }
-
             };
 
-            saxParser.parse("c:\\file.xml", handler);
+            saxParser.parse(xmlStream, handler);
 
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
+        } catch (IOException | ParserConfigurationException ex) {
+            Logger.getLogger(MySimpleXML.class.getName()).log(Level.SEVERE, null, ex);
+        } 
+        return root;
     }
-
 }
